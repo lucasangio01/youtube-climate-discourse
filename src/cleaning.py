ba7636c3b219e91d4a2d246127a4fb103701533a
lucasangio01 <luca.sangiovanni.2001@gmail.com>
2025-10-21 23:33:25 +0200

    from multiple methods to single cleaning class

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,48 +1,47 @@
 from deepmultilingualpunctuation import PunctuationModel
 from sentence_transformers import SentenceTransformer
 import pandas as pd
+import numpy as np
 
 
-punctuation_model = PunctuationModel()
-embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+class PreprocessText:
+    def __init__(self):
+        self.punctuation_model = PunctuationModel()
+        self.embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.chunk_size = 250
+        self.climate_concepts = ["climate change", "global warming", "carbon emissions", "green energy policies", "fossil fuel industry", "climate activism", "environmental regulation", "climate policy debate", "renewable energy transition", "carbon tax"]
+        self.climate_columns = ["clim_change", "glob_warm", "carb_emis", "green_en_policies", "fos_fu_ind", "clim_activ", "env_regul", "clim_polic_debate", "renew_en_trans", "carb_tax"]
 
-def split_into_chunks(text):
-    chunk_size = 250
-    words = text.split()
-    return [" ".join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
+    def split_into_chunks(self, text):
+        words = text.split()
+        return [" ".join(words[i:i + self.chunk_size]) for i in range(0, len(words), self.chunk_size)]
 
+    def create_punctuation(self, text):
+        return self.punctuation_model.restore_punctuation(text)
 
-def create_punctuation(text):
-    return punctuation_model.restore_punctuation(text)
+    def apply_chunking(self, videos_original):
+        videos_chunked = (videos_original.assign(chunks=videos_original["transcript"].apply(lambda text: self.split_into_chunks(text))).explode("chunks", ignore_index=True)).drop(columns = ["transcript"])
+        videos_chunked = videos_chunked.to_csv("../data/videos_chunked.csv", index = False)
+        return videos_chunked
 
+    def apply_punctuation(self, videos_chunked):
+        videos_punctuation = videos_chunked.copy()
+        videos_punctuation["chunks_punctuation"] = videos_punctuation["chunks"].apply(self.create_punctuation)
+        videos_punctuation = videos_punctuation.drop(columns=["chunks"])
+        videos_punctuation = videos_punctuation.to_csv("../data/videos_punctuation.csv", index = False)
+        return videos_punctuation
 
-def apply_chunking(videos_original):
-    videos_chunked = (videos_original.assign(chunks=videos_original["transcript"].apply(lambda text: split_into_chunks(text))).explode("chunks", ignore_index=True)).drop(columns = ["transcript"])
-    videos_chunked = videos_chunked.to_csv("../data/videos_chunked.csv", index = False)
-    return videos_chunked
+    def compute_similarity(self, videos_punctuation):
+        embedded_concepts = self.embedding_model.encode(self.climate_concepts)
+        embedded_chunks = self.embedding_model.encode(videos_punctuation["chunks_punctuation"])
+        similarities = self.embedding_model.similarity(embedded_concepts, embedded_chunks).tolist()
+        similarities_T = list(map(list, zip(*similarities)))
+        similarities_T_rounded = [np.round(x, 3) for x in similarities_T]
 
+        videos_similarity = videos_punctuation.copy()
+        videos_similarity_full = pd.DataFrame(similarities_T_rounded, columns=self.climate_columns, index=videos_similarity.index)
 
-def apply_punctuation(videos_chunked):
-    videos_punctuation = videos_chunked.copy()
-    videos_punctuation["chunks_punctuation"] = videos_punctuation["chunks"].apply(create_punctuation)
-    videos_punctuation = videos_punctuation.drop(columns=["chunks"])
-    videos_punctuation = videos_punctuation.to_csv("../data/videos_punctuation.csv", index = False)
-    return videos_punctuation
+        concat_df = pd.concat([videos_similarity, videos_similarity_full], axis=1).drop(["chunks_punctuation"], axis = 1)
+        concat_df.to_csv("videos_similarity.csv", index=False)
 
-
-def compute_similarity(videos_punctuation):
-    climate_concepts = ["climate change", "global warming", "carbon emissions", "green energy policies", "fossil fuel industry", "climate activism", "environmental regulation", "climate policy debate", "renewable energy transition", "carbon tax"]
-    climate_columns = ["clim_change", "glob_warm", "carb_emis", "green_en_policies", "fos_fu_ind", "clim_activ", "env_regul", "clim_polic_debate", "renew_en_trans", "carb_tax"]
-
-    embedded_concepts = embedding_model.encode(climate_concepts)
-    embedded_chunks = embedding_model.encode(videos_punctuation["chunks_punctuation"])
-    similarities = embedding_model.similarity(embedded_concepts, embedded_chunks).tolist()
-    similarities_T = list(map(list, zip(*similarities)))
-
-    videos_similarity = videos_punctuation.copy()
-    videos_similarity_full = pd.DataFrame(similarities_T, columns=climate_columns, index=videos_similarity.index)
-
-    concat_df = pd.concat([videos_similarity, videos_similarity_full], axis=1)
-    concat_df.to_csv("../data/videos_similarity.csv", index=False)
-
-    return concat_df
+        return concat_df
